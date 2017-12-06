a7d81cc5bfa7f291db1dea5ead7403aaa1a80156
Hugo <huhernandez@csumb.edu>
2017-12-06 20:09:12 +0000

    revised lab9

--- labs/lab9/index.php
+++ labs/lab9/index.php
@@ -1,8 +1,49 @@
 <?php
     include 'inc/header.php';
+
 ?>
-            
+  
+
+    
+</script> 
+          
+          
+          
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="img/charlie.jpg" alt="Charlie">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/carl.jpg" alt="Carl">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/samantha.jpg" alt="Samantha">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/ted.jpg" alt="Ted">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/sally.jpg" alt="Sally">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/alex.jpg" alt="Alex">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>  
         <!-- add Carousel component -->
+        
+        
+        
+        
         <br /><br />
         <a class="btn btn-outline-primary" href="adoptions.php" role="button">Adopt Now! </a>
 
