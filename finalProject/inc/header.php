c7273ad01979e8e28a7286a85d7ac35c81afc8d3
Hugo <huhernandez@csumb.edu>
2017-12-08 19:29:57 +0000

    final modify header

--- finalProject/inc/header.php
+++ finalProject/inc/header.php
@@ -3,7 +3,7 @@
     <head>
         
     
-        <title> GamesGalore </title>
+        <title> GameInferno </title>
        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
